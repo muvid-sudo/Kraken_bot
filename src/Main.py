95819947c88b65cb6f6e8eeee27ce6ce41750540
Timofey Dankevich <Timofey.Dankevich@gmail.com>
2021-07-26 11:59:13 +0300

    I've splited a program by several modules. There are API, DataCollection and DataOutput modules. This module is an entry point of the program

---
+++ src/Main.py
@@ -0,0 +1,44 @@
+import time
+from email import utils
+import DataCollection as dc
+import DataOutput as do
+
+
+def find_time(time):
+    date = utils.parsedate_to_datetime(time)
+    #date.strftime('%d/%b/%Y')
+    return date.strftime('%H:%M')
+
+
+def get_price_period(pair, period):
+    timestamps = []
+    prices = []
+    t_end = time.time() + 60 * period
+
+    previous_price = -1
+    while time.time() < t_end:
+        current_price = float(dc.get_current_price(pair))
+
+        timestamp = find_time(time.asctime(time.localtime(time.time())))
+
+        if current_price != previous_price and current_price > 0:
+            timestamps.append(timestamp)
+            prices.append(current_price)
+
+        previous_price = current_price
+
+    time_and_price = {'Time': timestamps, 'Price': prices}
+
+    return time_and_price
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    # dictionary, key([time]) -> value([price])
+    quotations = get_price_period('ETHUSDT', 30)
+
+    # to show a price graph
+    do.get_graph(quotations)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
