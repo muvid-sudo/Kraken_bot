4f3fa36f66530812918fe159731a62426c36eda7
Timofey Dankevich <Timofey.Dankevich@gmail.com>
2021-08-07 01:10:05 +0300

    We did collection of prices and writing to a file

--- src/Main.py
+++ src/Main.py
@@ -8,40 +8,54 @@ import pandas as pd
 import os
 
 
-def add_data_to_file(path, current_price):
-    df = pd.read_csv(path, 'a+', header=1, names=['time', 'open', 'high', 'low', 'close'])
-    print(df.head)
+def add_data_to_file(path, updated_data):
+    df = pd.read_csv(path)
     last_time = df.at[df.index[-1],'time']
-    print(last_time, current_price[0])
-    #if int(last_time) < int(current_price[0]):
-    #    df.append(current_price)
-    #    df.to_csv(path, encoding='utf-8', index=False, columns=['time', 'open', 'high', 'low', 'close'], header=True)
+    for row in updated_data: 
+        rows = [pd.Series(row, index=df.columns)]
+        df = df.append(rows, ignore_index=True)
+    df.to_csv(path, encoding='utf-8', index=False, columns=['time', 'open', 'high', 'low', 'close'], header=True)
+
+
+def create_file(filename):
+    path = '../Data/%s' % filename
+    if not os.path.isfile(path):
+        prices = open(path, 'w+')
+    return path 
+
+
+def get_new_data(curr_data, prev_data):
+    curr_data = pd.DataFrame(curr_data, columns=['time', 'open', 'high', 'low', 'close', '6', '7', '8'])
+    curr_data = curr_data.drop(columns=['6', '7', '8'])
+
+    updated_data = []
+
+    length_prev = len(prev_data)
+    length_curr = len(curr_data)
+
+    start = length_prev - length_curr
+
+    for row in range(length_curr):
+        if prev_data.values[start + row][0] != curr_data.values[row][0] and prev_data.values[start + row][0] < curr_data.values[row][0]:
+            updated_data.append(curr_data.values[row])
+
+    return updated_data
 
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    path_to_file = '../Data/price.csv'
-    if not os.path.isfile(path_to_file):
-        prices = open(path_to_file, 'w+')
+    path = create_file('price.csv')    
 
     while True:
-        price = dc.pair_price('ETHUSDT')
-
-        current_price = [
-                price[-1][0],
-                price[-1][1],
-                price[-1][2],
-                price[-1][3],
-                price[-1][4]
-                ]
-
-        if os.path.exists(path_to_file) and os.stat(path_to_file).st_size != 0:
-            add_data_to_file(path_to_file, current_price)
+        current_data = dc.pair_price('ETHUSDT')
+        
+        if os.path.exists(path) and os.stat(path).st_size != 0:
+            previous_data = pd.read_csv(path)
+            new_data = get_new_data(current_data, previous_data)
+            add_data_to_file(path, new_data)
         else:
-            df = pd.DataFrame(price, columns=['time', 'open', 'high', 'low', 'close', '6', '7', '8'])
-            df = df.drop(columns=['6', '7', '8'])
-            df.to_csv(path_to_file, encoding='utf-8', index=False, columns=['time', 'open', 'high', 'low', 'close'], header=True)
+            current_data.to_csv(path, encoding='utf-8', index=False, columns=['time', 'open', 'high', 'low', 'close'], header=True)
         time.sleep(30)
 
     print("--- %s seconds ---" % (time.time() - start_time))
