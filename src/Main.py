49618495b8d55a41546730320d0cba2d95953125
Timofey Dankevich <Timofey.Dankevich@gmail.com>
2021-08-08 02:14:08 +0300

    Sorted the code in functions

--- src/Main.py
+++ src/Main.py
@@ -6,12 +6,11 @@ from email import utils
 import datetime as dt
 import pandas as pd
 import os
-
+import DataAnalyse as da
 
 def add_data_to_file(path, updated_data):
     df = pd.read_csv(path)
-    last_time = df.at[df.index[-1],'time']
-    for row in updated_data: 
+    for row in updated_data:
         rows = [pd.Series(row, index=df.columns)]
         df = df.append(rows, ignore_index=True)
     df.to_csv(path, encoding='utf-8', index=False, columns=['time', 'open', 'high', 'low', 'close'], header=True)
@@ -20,13 +19,18 @@ def add_data_to_file(path, updated_data):
 def create_file(filename):
     path = '../Data/%s' % filename
     if not os.path.isfile(path):
-        prices = open(path, 'w+')
+        price = open(path, 'w+')
     return path 
 
 
+def edit_price_data(current_data):
+    current_data = pd.DataFrame(current_data, columns=['time', 'open', 'high', 'low', 'close', '6', '7', '8'])
+    current_data = current_data.drop(columns=['6', '7', '8'])
+    return current_data
+
+
 def get_new_data(curr_data, prev_data):
-    curr_data = pd.DataFrame(curr_data, columns=['time', 'open', 'high', 'low', 'close', '6', '7', '8'])
-    curr_data = curr_data.drop(columns=['6', '7', '8'])
+    curr_data = edit_price_data(curr_data)
 
     updated_data = []
 
@@ -42,20 +46,31 @@ def get_new_data(curr_data, prev_data):
     return updated_data
 
 
+def collect_price_data(path, pair):
+    #'ETHUSDT'
+    current_data = dc.pair_price(pair)
+    if os.path.exists(path) and os.stat(path).st_size != 0:
+        previous_data = pd.read_csv(path)
+        new_data = get_new_data(current_data, previous_data)
+        add_data_to_file(path, new_data)
+    else:
+        edit_price_data(current_data)
+        current_data.to_csv(path, encoding='utf-8', index=False, columns=['time', 'open', 'high', 'low', 'close'], header=True)
+
+
 if __name__ == '__main__':
     start_time = time.time()
 
-    path = create_file('price.csv')    
+    path = create_file('price.csv')
 
     while True:
-        current_data = dc.pair_price('ETHUSDT')
-        
-        if os.path.exists(path) and os.stat(path).st_size != 0:
-            previous_data = pd.read_csv(path)
-            new_data = get_new_data(current_data, previous_data)
-            add_data_to_file(path, new_data)
-        else:
-            current_data.to_csv(path, encoding='utf-8', index=False, columns=['time', 'open', 'high', 'low', 'close'], header=True)
+        collect_price_data(path, 'ETHUSDT')
+        data = pd.read_csv(path)
+        FMA, SMA = da.moving_average(data, fast_period=5, slow_period=10)
+        print(da.comparison_ma(FMA, SMA))
         time.sleep(30)
+
+    # do.some_graph(data)
+       #do.get_candle_graph(data)
 
     print("--- %s seconds ---" % (time.time() - start_time))
