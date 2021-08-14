618ab956a6beffbfa7810f9d1ee2164e0daf6f8f
Timofey Dankevich <Timofey.Dankevich@gmail.com>
2021-08-15 01:27:58 +0300

    Deleted unneeded methods

--- src/TradeStrategy.py
+++ src/TradeStrategy.py
@@ -1,21 +1,29 @@
 import DataAnalyse as da
-import DataPreprocessing as dp
+import DataPreprocessor as dp
 
 
+# The method builds a current moving average.
+def SMA(data, price='open', period=10):
+    SMA = 0.0
+
+    num_of_rows = len(data)
+    learned_start = num_of_rows - period
+
+    sum_for_period = 0
+    for val in data[price].values[learned_start:num_of_rows]:
+        sum_for_period += float(val)
+    SMA = sum_for_period / period 
+
+    return SMA 
 
 
+# The method builds a current exponential moving average
+def CEMA(data, LEMA, price='open', period=10, smoothing=2):
+    EMA = 0.0
 
-def comparison_ma(FMA, SMA):
-    if FMA.values[-1] == SMA.values[-1] and FMA.values[-2] > SMA.values[-2]:
-        return 'sell'
-    elif FMA.values[-1] == SMA.values[-1] and FMA.values[-2] < SMA.values[-2]:
-        return 'buy' 
+    num_of_rows = len(data)
+    smoothing_factor = smoothing / period + 1
 
+    EMA = (data[price].values[-1] * smoothing_factor) + LEMA * (1 - smoothing_factor)
 
-def analyze_current_moving_average(pair, price='close', fast_period=10, slow_period=30):
-    ohlc = get_ohlc(pair)
-    fma = da.SMA(ohlc, price, fast_period)
-    sma = da.SMA(ohlc, price, slow_period)
-
-
-
+    return EMA
