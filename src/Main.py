1415a15f0ccadba65d7147b07e592f6bd5ecab1a
Timofey Dankevich <Timofey.Dankevich@gmail.com>
2021-08-20 00:48:40 +0300

    Implemented trading test

--- src/Main.py
+++ src/Main.py
@@ -5,12 +5,19 @@ import DataPreprocessor as dp
 import TradeStrategy as ts
 import Caching as cache
 
+from Simulation import Simulation as sm
 
 if __name__ == '__main__':
     start_time = time.time()
 
+    pair = 'ETHUSDT'
+    
+    account = sm(100000)
+    
+    budget = account.get_budget()
+    print('Primary budget', budget)
+
     previous_time = 0
-    SF_SMA = []
     prev_ssma = 0
     prev_fsma = 0
     while True:
@@ -21,13 +28,24 @@ if __name__ == '__main__':
         if ohlc['time'].values[-1] != previous_time:
             SSMA = ts.SMA(ohlc, 'close', 120)
             FSMA = ts.SMA(ohlc, 'close', 60)
-            ts.comparison_ma(prev_ssma, SSMA, prev_fsma, FSMA)
+
+            signal = ts.comparison_ma(prev_ssma, SSMA, prev_fsma, FSMA)
+           
+            if signal == -1 or signal == 1:
+                order = ts.creation_of_order(budget, 5, signal, pair)
+                account.add_position(order)
+           
+                positions = account.get_positions()
+
+                cache.creation_or_updation_table_file('Orders.csv', order.get_position(), ['id', 'Pair', 'Price', 'Volume', 'Type', 'Ordertype', 'Time', 'Position_cost'])
+
+                order.info_order()
+                print()
+
             prev_ssma = SSMA
             prev_fsma = FSMA
-            SF_SMA.append([SSMA, FSMA])
-            cache.creation_or_updation_table_file('ETH_SMA.csv', SF_SMA, ['SMA_120', 'SMA_60'])
-        if len(SF_SMA) == 1000:
-            SF_SMA.clear()
+            cache.creation_or_updation_table_file('ETH_SMA.csv', [[SSMA, FSMA]], ['SMA_120', 'SMA_60'])
+
         previous_time = ohlc['time'].values[-1]
         time.sleep(30)
 
